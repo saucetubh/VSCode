d93c44fe6148a26c67e52fd39ce5b5edc1f01f90
saucetubh <f20230694@goa.bits-pilani.ac.in>
2025-11-06 22:07:34 +0530

    Iterators

--- OOPs/Practice/postmidsem/DS/Main.java
+++ OOPs/Practice/postmidsem/DS/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 public class Main {
     public static void main(String[] args) {
         MyQueue q = new MyQueue();
@@ -25,6 +30,52 @@ public class Main {
         s.push(400);
         System.out.println("Top element: " + s.peek()); // 400
         System.out.println("Stack empty? " + s.isEmpty()); // false
+        //List<Integer> l = new ArrayList<>(); //list is an interface, to instantiate an object of reference type List we must use a class that implements the interface
+        //although we can use ArrayList as the reference type too, but this is preferred since now we can use methods where any type of List is expected (Linked or Array)
+        //example
+        // This method is flexible. It only cares that it gets a "List".
+        List<String> al = new ArrayList<>();
+        al.add("a");
+        al.add("b");        
+        List<String> ll = new LinkedList<>();
+//methods i can call depends on LHS i.e in this case only the methods deifned in list interface
+//version of the methods implemented depends on the RHS object type (run time polymorphism, overriding methods)
+        ll.add("x");
+        ll.add("y");        
+        printList(al);    // Output: a, b
+        printList(ll);   // Output: x, y
 
+        List<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+        names.add(2, "Diana"); // Inserting at index 2
+        printList(names); // Output: Alice, Bob, Diana, Charlie
+        names.remove(3); // Removing element at index 3
+        printList(names); // Output: Alice, Bob, Diana
+        names.set(0, "Eve"); // Updating element at index 0
+        names.remove(names.size() - 1); // Removing last element
+        printList(names); // Output: Eve, Bob
+        iter(names);
+        iter2(names);
     }
+
+    public static void printList(List<String> anyList) {
+        for (String s : anyList) {
+            System.out.println(s);
+        }
+    }
+    public static void iter(List<String> anyList) {
+        Iterator<String> it = anyList.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+    public static void iter2(List<String> anyList) {
+        ListIterator<String> it = anyList.listIterator();
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+    //ListIterator can do stuff like it.next, it.previous, it.add, it.remove
 }
---
+++ OOPs/Practice/postmidsem/DS/Main2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main2 {
+    public static void main(String[] args) {
+        List<String> names = new LinkedList<>();
+        List<Integer> marks = new ArrayList<>();
+        //ListIterator<String> it1 = names.listIterator(); this will not work, iterator is synced to an empty list. Iterator must be defined after list is modified
+        //ListIterator<Integer> it2 = marks.listIterator();
+        names.add("Bob");
+        marks.add(0);
+        marks.add(0);
+        marks.add(0);
+        marks.add(0);
+        marks.add(0);
+        names.add("John");
+        names.add("Mary");
+        names.add("Juan");
+        names.add("Lana");
+        marks.set(names.indexOf("John"), 20);
+        marks.set(names.indexOf("Lana"), 70);
+        marks.set(names.indexOf("Juan"), 100);
+        marks.set(names.indexOf("Mary"), 50);
+        ListIterator<String> it1 = names.listIterator();
+        ListIterator<Integer> it2 = marks.listIterator();
+        it1.add("Doe"); //iterator is before index 0 when created, so adding here adds at index 0, and now index is between 0 and 1 so it starts printing from index 1 unless we do it1.previous();
+        it1.next();
+        it1.next();
+        it1.add("Anna");
+        it1.next();
+        it1.remove();
+        it1 = names.listIterator(); //reset iterator to start
+        while(it1.hasNext()) {
+            System.out.println(it1.next());
+        }
+        for(Integer i : marks) {
+            System.out.println(i);
+        }
+        //think of iterator as your cursor in a text editor
+    }
+}
